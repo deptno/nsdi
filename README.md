0437b0e094aa0e82730eccb41a40b8c79d5a22ea
deptno <deptno@gmail.com>
2019-12-23 19:16:59 +0900

    chore: module 지원

--- README.md
+++ README.md
@@ -5,14 +5,22 @@
 
 현재 변동 데이터만을 지원
 
+### 실행
+npx
 ```shell script
 npx nsdi --help
 npx nsdi -t 연속지적도형정보 -s 20191219 -f shp
+```
+```shell script
+npm -g install nsdi # 글로벌 설치
+nsdi --help
+nsdi -t 연속지적도형정보 -s 20191219 -f shp
 ```
 
 예제
 ```shell script
   npx nsdi -t 연속지적도형정보 -s 20191219 -f shp
+Downloading Chromium r706915 - 111.8 Mb [====================] 100% 0.0s 
 대상 파일 수: 1
-1/1 완료, CH_00_D002_20191221.zip(5328452 바이트)
+......1/1 완료, CH_00_D002_20191221.zip(5328452 바이트)
 ```
--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,26 @@
-#!/usr/bin/env node
-
-import program from 'commander'
-import {readFileSync} from 'fs'
-import {list} from './list'
 import {page} from './lib/page'
 import {download} from './download'
 import {assertDate8, assertFile, assertString} from './lib/asserts'
 import {removeCrDownloads} from './lib/remove-crdownload'
 
-async function main() {
-  const pkg = JSON.parse(readFileSync('package.json').toString())
-
-  program
-    .version(pkg.version)
-    .option('-l, --list', '다운로드 가능한 목록')
-    .option('-t, --type <value>', '연속지적도형정보')
-    .option('-f, --file <value>', 'shp OR csv')
-    .option('-s, --starts-with <value>', '> yyyyMMdd')
-    .parse(process.argv)
-
+async function nsdi({type, file, startsWith}) {
   const [b, p] = await page()
 
   try {
-    if (program.list) {
-      await list(p)
-    } else if (program.type) {
-      const {type, file, startsWith} = program
+    assertDate8(startsWith)
+    assertFile(file)
+    assertString(type)
 
-      assertDate8(startsWith)
-      assertFile(file)
-      assertString(type)
-
-      await removeCrDownloads()
-
-      if (startsWith) {
-        await download(p, {
-          startsWith: startsWith + ' ',
-          file,
-          type,
-        })
-      }
-    } else {
-      program.help()
-    }
+    await removeCrDownloads()
+    await download(p, {
+      startsWith: startsWith + ' ',
+      file,
+      type,
+    })
   } catch (e) {
     console.error(e)
-    program.help()
   } finally {
     await b.close()
   }
 }
 
-main()
